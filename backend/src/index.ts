4d7151b62052d9da762a1ab5a1fcda75586b22f1
Ruslan <free.changer@gmail.com>
2022-11-18 07:47:13 +0000

    Added frontend files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,23 @@
 import * as http from 'http'
+import * as path from 'path'
 import express from 'express'
+import morgan from 'morgan'
 import { Server } from 'socket.io'
 
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server);
+const io = new Server(server)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-});
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+    console.log('a user connected')
+})
 
 server.listen(port, () => {
-    console.log(`listening on *:${port}`);
-});
+    console.log(`listening on *:${port}`)
+})
 
 
